338ecabafb019c86200265a37b51d9e09f6663c9
Angelo Munar <91932024+geloNaru@users.noreply.github.com>
2025-06-10 22:57:06 +0800

    feat: added content such as navbar and footer on default layout and ehance the rocket view UI

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-footer app>
+		<v-container>
+			<v-row>
+				<v-col class="text-center">
+					<p>&copy; {{ new Date().getFullYear() }} Angelo Munar | Frontend Test Apollo Lights</p>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-footer>
+</template>
+
+<script>
+export default {
+	name: 'AppFooter',
+}
+</script>
+
+<style scoped>
+/* Add any specific styles for the footer here */
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,24 @@
 <template>
 	<v-app>
+		<ClientOnly>
+			<AppNavbar />
+		</ClientOnly>
 		<v-main>
 			<slot />
 		</v-main>
+		<AppFooter />
 	</v-app>
 </template>
+
 <script>
+import AppFooter from '~/components/AppFooter.vue'
+import AppNavbar from '~/components/AppNavbar.vue'
+
 export default {
 	name: 'DefaultLayout',
+	components: {
+		AppNavbar,
+		AppFooter,
+	},
 }
 </script>
